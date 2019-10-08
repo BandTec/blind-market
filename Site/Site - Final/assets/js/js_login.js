cd09319162e4957b0f907ff97d77785cf066b7e8
João Lucas <joao.jacintho@bandtec.com.br>
2019-10-08 13:08:45 -0300

    Tela de login finalizada

--- Site/Site - Final/assets/js/js_login.js
+++ Site/Site - Final/assets/js/js_login.js
@@ -1,8 +1,16 @@
-var telinha = tela_login;
+var telaLogin = tela_login;
 var cadastro = criar_conta;
 
-function mostrar()
-{
-    mostrar_cadastro.display = 'block';
-    tela_login.display = 'none';
-}
+var telaCadastro = mostrar_cadastro;
+var login = fazer_login;
+
+cadastro.addEventListener("click", function () {
+    telaLogin.classList.add('invisivel')
+    telaCadastro.classList.remove('invisivel')
+})
+
+login.addEventListener("click", function () {
+    telaLogin.classList.remove('invisivel')
+    telaCadastro.classList.add('invisivel')
+})
+
