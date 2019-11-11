528bf9dc51401f0b2a9914eb3350bd9abac1ccc6
arthuralvaresbandtec <arthur.alvares@bandtec.com.br>
2019-11-11 17:32:46 -0200

    script criacao no azure

---
+++ BD/banco-blindmarket-azure.sql
@@ -0,0 +1,64 @@
+create database blind_market;
+use blind_market;
+
+/* criando tabelas */
+
+create table empresa (
+idempresa int primary key identity(1,1),
+nome varchar(50),
+login varchar(50),
+email varchar(50),
+senha varchar(50)
+);
+
+create table estabelecimento (
+idestabelecimento int primary key identity(1,1),
+nome varchar(50),
+endereco varchar(50),
+cep char(9)
+);
+
+create table categoria (
+idcategoria int primary key identity(1,1),
+nome varchar(50)
+);
+
+create table registro(
+idregistro int primary key identity(1,1),
+datahora datetime
+);
+
+create table sensor (
+idsensor int primary key identity(1,1),
+corredor varchar(50),
+porta varchar(50)
+);
+
+create table produto (
+idproduto int primary key identity(1,1),
+nome varchar(50),
+preco decimal(5,2)
+);
+
+select * from produto;
+select * from empresa;
+select * from estabelecimento;
+select * from registro;
+select * from categoria;
+select * from sensor;
+
+/* criando colunas fk*/
+
+alter table produto add fkCategoria int;
+alter table produto add foreign key (fkCategoria) references categoria (idcategoria);
+
+alter table estabelecimento add fkEmpresa int;
+alter table estabelecimento add foreign key (fkEmpresa) references Empresa (idempresa);
+
+alter table registro add fkSensor int;
+alter table registro add fkProduto int;
+alter table registro add foreign key (fkSensor) references Sensor (idsensor);
+alter table registro add foreign key (fkProduto) references Produto (idproduto);
+
+alter table sensor add fkProduto int;
+alter table sensor add foreign key (fkProduto) references Produto (idProduto);
