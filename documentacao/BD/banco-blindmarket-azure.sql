e04fb6a59be3285fd705eaac305c436c7e0a2066
Manoel Almeida <manoel.almeida@bandtec.com.br>
2019-11-13 23:57:21 -0300

    Selects dos gráficos estatísticos da página inicial da dashboard

--- documentacao/BD/banco-blindmarket-azure.sql
+++ documentacao/BD/banco-blindmarket-azure.sql
@@ -4,40 +4,40 @@ use blind_market;
 /* criando tabelas */
 
 create table empresa (
-idempresa int primary key identity(1,1),
-nome varchar(50),
-login varchar(50),
-email varchar(50),
-senha varchar(50)
+	idempresa int primary key identity(1,1),
+	nome varchar(50),
+	login varchar(50),
+	email varchar(50),
+	senha varchar(50)
 );
 
 create table estabelecimento (
-idestabelecimento int primary key identity(1,1),
-nome varchar(50),
-endereco varchar(50),
-cep char(9)
+	idestabelecimento int primary key identity(1,1),
+	nome varchar(50),
+	endereco varchar(50),
+	cep char(9)
 );
 
 create table categoria (
-idcategoria int primary key identity(1,1),
-nome varchar(50)
+	idcategoria int primary key identity(1,1),
+	nome varchar(50)
 );
 
 create table registro(
-idregistro int primary key identity(1,1),
-datahora datetime
+	idregistro int primary key identity(1,1),
+	datahora datetime
 );
 
 create table sensor (
-idsensor int primary key identity(1,1),
-corredor varchar(50),
-porta varchar(50)
+	idsensor int primary key identity(1,1),
+	corredor varchar(50),
+	porta varchar(50)
 );
 
 create table produto (
-idproduto int primary key identity(1,1),
-nome varchar(50),
-preco decimal(5,2)
+	idproduto int primary key identity(1,1),
+	nome varchar(50),
+	preco decimal(5,2)
 );
 
 select * from produto;
@@ -62,3 +62,16 @@ alter table registro add foreign key (fkProduto) references Produto (idproduto);
 
 alter table sensor add fkProduto int;
 alter table sensor add foreign key (fkProduto) references Produto (idProduto);
+alter table sensor add fkEstabelecimento int;
+alter table sensor add foreign key (fkEstabelecimento) references Estabelecimento (idestabelecimento);
+
+/* Selects para compor os gráficos estatísticos */
+
+-- Quantidade de sensores ativos de todos os estabelecimentos
+select DATEPART(Year, datahora) ano, DATEPART(Month, datahora) as mes, count(idregistro) as qtd from registro, sensor, estabelecimento where fkSensor = idsensor and fkEstabelecimento = idEstabelecimento and fkEmpresa = 1 and datahora >= DATEADD(Month, -12, getdate()) group by DATEPART(Year, datahora), DATEPART(Month, datahora) order by ano, mes;
+
+-- Quantidade de sensores ativos por categoria de produto
+select categoria.nome, count(idregistro) qtd from registro, sensor, estabelecimento, produto, categoria where registro.fkProduto = idproduto and fkCategoria = idcategoria and fkSensor = idsensor and fkEstabelecimento = idEstabelecimento and fkEmpresa = 1 group by categoria.nome;
+
+-- Quantidade de sensores ativos por estabelecimento
+select nome, count(idregistro) from registro, sensor, estabelecimento where fkSensor = idsensor and fkEstabelecimento = idEstabelecimento and fkEmpresa = 2 group by nome;
